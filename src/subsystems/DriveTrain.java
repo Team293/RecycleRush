9d75273b08cf3694cacdc401b79689cdb090cc77
Team293 <293.First@gmail.com>
2015-03-31 15:52:50 -0400

    :circus_tent: Navruz
    Landfill auto, gyroDrive

--- src/subsystems/DriveTrain.java
+++ src/subsystems/DriveTrain.java
@@ -2,6 +2,7 @@ package subsystems;
 
 import org.usfirst.frc.team293.robot.Ports;
 
+import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.VictorSP;
 
@@ -10,7 +11,11 @@ public class DriveTrain {
     private static final VictorSP rightMotor = new VictorSP(Ports.rightDrive);
     
     private static final RobotDrive drive = new RobotDrive(leftMotor, rightMotor);
+    
+    private static final Gyro gyro = new Gyro(Ports.gyro);
 
+    private static final double kP = 0.008;
+    
     public static void tankDrive(double leftSpeed, double rightSpeed) {
 		drive.tankDrive(leftSpeed, rightSpeed);
 	}
@@ -19,12 +24,14 @@ public class DriveTrain {
     	drive.tankDrive(leftSpeed * 0.5, rightSpeed * 0.5);
     }
 	
-	public static void skidControl(double leftInput, double rightInput) {
-		
-	}
-	
 	public static void arcadeDrive(double move, double rotate) {
 		drive.arcadeDrive(move, rotate);
+	}
+	
+	public static void gyroDrive(double setAngle, double speed) {
+		double error = setAngle - gyro.getAngle();
+		double difference = error * kP;
+		tankDrive(speed - difference, speed + difference);
 	}
 	
 	public static void stop() {
